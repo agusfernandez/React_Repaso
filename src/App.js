66500bb36bf7d56a76b5f85b2bd71962f3461412
Maria Agustina Fernandez <agustina.fer91@gmail.com>
2021-08-29 23:42:36 -0300

    new state name

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './components/Counter'
+import {useState} from 'react';
+
 // import { Container, Row } from "react-bootstrap";
 // pasar el prop
 
 function App() {
 
   const mensajeBienvenida = <h2>bienvenido al repaso</h2>
-  const nombre = "Agustina";
-// lo que paso por nombre en el first name tiene q llamarse igual q la variable
-// al nombde de la propiedad (fistname le puedo poner cualquier nombre pero es una buena practica  q la prop tenga el mismo nombre al que le voya  asignar)
-  return (
+  //const nombre = "Agustina";
+
+  // lo que paso por nombre en el first name tiene q llamarse igual q la variable
+  // al nombde de la propiedad (fistname le puedo poner cualquier nombre pero es una buena practica  q la prop tenga el mismo nombre al que le voya  asignar)
+  
+  // si quiero cambiar el estado de un nombre cuando haga algo en un componente hijo entonce....
+  const [nombre, setNombre] = useState("Agustina");
+
+return (
+
+  // le tengo q pasar el setNombre para que cambie el estado en el counter.js
     <>      
     {mensajeBienvenida}
-  
-    <Counter firstName={nombre}/>
+    <h2> Otra forma de setear el estado del nombre: {nombre}</h2>
+    <Counter nombre={nombre} setNombre={setNombre}/>
     </>
   );
 }
--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,17 +1,26 @@
 // le podemos pasar datos de otro lado y a ello se le llama props ( a los datos)
 import { useState } from "react";
 
-const Counter =(props)=>{
+//{nombre} > le paso la propiedad nombre desestructurada ya que es un objeto
+const Counter =({nombre, setNombre})=>{
 
-    const [count, setCount] = useState();
+    const [count, setCount] = useState(8);
     const increment=()=>{
-        setCount((prevCount)=>prevCount+1);
+         setCount((prevCount)=>prevCount+1);
+         setNombre('Margarita');
+     }
+     const decrement=()=>{
+        setCount((prevCount)=>prevCount-1);
+        setNombre('Javier');
+
     }
     return(
         <>
-            <h3>El nombre del desarrollador es {props.firstName}</h3>
-        
-        
+            <h3>El nombre del desarrollador es {nombre}</h3>
+            <button onClick={()=>increment()}>+</button>
+            <span>{count}</span>
+            <button onClick={()=>decrement()}>-</button>
+
         </>
     );
 }
