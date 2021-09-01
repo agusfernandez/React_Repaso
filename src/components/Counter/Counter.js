c7fcbc5951f6116ac6ef1a656a5287a11e986d68
Maria Agustina Fernandez <agustina.fer91@gmail.com>
2021-09-01 00:10:31 -0300

    Nuevos hooks

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -12,7 +12,19 @@ const Counter =({nombre, setNombre})=>{
         return 5;
     }
 
-    const [count, setCount] = useState(()=>momentoInicial());
+    const [count, setCount] = useState(()=>momentoInicial()); // le paso el callback
+    const [state, setState] = useState({
+            inicio:0,
+            name: 'Agustina'
+    })
+
+    const iniciar= state.inicio;
+    const name = state.name;
+
+    // otro tipo de hook
+    const [change, setChange] =useState (0);
+    const [surname, setSurname] = useState("Fernandez");
+
 
     const increment=()=>{
         // el prevCount es una funcion q permite traer el estado viejo +1 y la estruciuracion de abajo indica q es el estado previo
@@ -22,14 +34,56 @@ const Counter =({nombre, setNombre})=>{
      const decrement=()=>{
         setCount((prevCount)=>prevCount-1);
         setNombre('Javier');
-
     }
+
+    ///////////////////////////////
+    const incrementTwo=()=>{
+        setState({
+            ...state,
+            //entras  al prop inicio
+           inicio: state.inicio + 1
+        })
+
+     }
+     const decrementTwo=()=>{
+        setState({
+            ...state,
+           inicio: state.inicio - 1
+        })
+    }
+
+
+    ////////////////////////////////
+    const incrementThree=()=>{
+        setChange(prevCount => prevCount +1)
+
+     }
+     const decrementThree=()=>{
+        setChange(prevCount => prevCount -1)
+    }
+
+
+
+
     return(
         <>
             <h3>El nombre del desarrollador es {nombre}</h3>
             <button onClick={()=>increment()}>+</button>
             <span>{count}</span>
             <button onClick={()=>decrement()}>-</button>
+
+            <h3>Otro tipo de renderizado</h3>
+            <button onClick={()=>incrementTwo()}>+</button>
+            <span>{iniciar}</span>
+            <span>{name}</span>
+            <button onClick={()=>decrementTwo()}>-</button>
+
+            <h3>Dos hooks</h3>
+            <button onClick={()=>incrementThree()}>+</button>
+            <span>{change}</span>
+            <span>{surname}</span>
+            <button onClick={()=>decrementThree()}>-</button>
+
 
         </>
     );
