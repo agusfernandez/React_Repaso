72b83f24e05bad83d3f87cf339be0bf2e42bd2f6
Maria Agustina Fernandez <agustina.fer91@gmail.com>
2021-09-13 00:32:24 -0300

    useRef

--- src/components/NameInput/NameInput.js
+++ src/components/NameInput/NameInput.js
@@ -6,10 +6,16 @@ import {Container, Row, Col, Button, Card, FormControl} from 'react-bootstrap';
 const NameInput =()=>{
     const [nombre, setNombre] = useState("");
     const renderName = useRef(1);
+    const useFocus = useRef();
 
     useEffect (()=>{
         renderName.current = renderName.current +1;
     }, [nombre]);
+
+    // permito hacer foco a un input a traves del DOM
+    function focus(){
+         useFocus.current.focus();
+    }
 
     return(
         <>
@@ -19,13 +25,14 @@ const NameInput =()=>{
                         <Card.Body>
                             <Row>
                                 <Col lg={4}>
-                                    <FormControl name={nombre} onChange={(e)=>setNombre(e.target.value)}/>
+                                    <FormControl ref ={useFocus} name={nombre} onChange={(e)=>setNombre(e.target.value)}/>
+                                    <Button onClick ={focus}>Focus</Button>
                                 </Col>
                                 <Col lg={4}>
                                     <span>Bienvenido {nombre}</span>
                                 </Col>
                                 <Col lg={4}>
-                                    <span>El componente se renderizo  {renderName.current} veces</span>
+                                    <span className="render-text">El componente se renderizo  {renderName.current} veces</span>
                                 </Col>
                             </Row>
                         </Card.Body>
